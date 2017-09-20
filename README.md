d5e34eaebb2c6fc3d03841a0a66d4ac953d4a554
Jiyoung Oh <jo289@cornell.edu>
2017-09-20 06:31:51 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Logs Analysis
+
+----
+## Overview
+>a Python program reporting tool that prints out reports based on the data in the database. The program runs from the command line. It does not take any user input. Instead, it uses the psycopg2 module to connect to the database, SQL queries to analyze the log data, print out the answers to three questions.
+
+----
+## Set Up
+
+####1. Install VirtualBox [here](https://www.virtualbox.org/wiki/Downloads
+
+####2. Install Vagrant [here](https://www.vagrantup.com/downloads.html)
+
+####3. Download the VM configuration.
+* Download and unzip this [file](https://d17h27t6h515a5.cloudfront.net/topher/2017/August/59822701_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+
+####4. Launch the Virtual Machine:
+From your terminal, inside the vagrant subdirectory, run the command
+
+    vagrant up
+
+When finished, run
+
+    vagrant ssh
+
+to log in to the newly installed Linux VM
+
+#### 5. Load the data
+
+cd into the vagrant directory and use the command
+
+    psql -d news -f newsdata.sql
+
+#### 6. Run the program on terminal
+
+    python3 logs_analysis.py
+
+----
+## Report
+
+The reporting tool answers the following questions.
+
+* **1. What are the most popular three articles of all time?** Which articles have been accessed the most? Present this information as a sorted list with the most popular article at the top.
+
+* **2. Who are the most popular article authors of all time? **That is, when you sum up all of the articles each author has written, which authors get the most page views? Present this as a sorted list with the most popular author at the top.
+
+* **3. On which days did more than 1% of requests lead to errors?**  The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser. (Refer back to this lesson if you want to review the idea of HTTP status codes.)
+
+
+----
+##Source
+* Udacity - Full Stack Developer Nanodegree
---
+++ logs_analysis.py
@@ -0,0 +1,72 @@
+# "Database code" for Logs Analysis
+import psycopg2
+import datetime
+# import bleach
+
+DBNAME = "news"
+
+# 1. What are the most popular three articles of all time?
+query1 = '''SELECT ARTICLE_VIEWS.title, views
+            FROM(SELECT articles.title, count(*) AS views
+                FROM articles JOIN log ON log.path LIKE '%'||articles.slug
+                GROUP BY articles.title) AS ARTICLE_VIEWS
+            ORDER BY views DESC
+            LIMIT 3;
+         '''
+
+# 2. Who are the most popular article authors of all time?
+query2 = '''SELECT authors.name, sum(views) as total_views
+          FROM authors JOIN
+          (SELECT articles.author, articles.title, count(*) AS views
+                FROM articles JOIN log ON log.path LIKE '%'||articles.slug
+                GROUP BY articles.author, articles.title) AS article_views
+          ON authors.id=article_views.author
+          GROUP BY authors.id
+          ORDER BY total_views DESC;
+       '''
+
+# 3. On which days did more than 1% of requests lead to errors?
+query3 = '''SELECT allrequests.date,
+           round(error.total_error*100/allrequests.total_requests,2)
+           as error_percentage
+           FROM (SELECT date(time) as date, count(*)
+                 as total_requests FROM log GROUP BY date) as allrequests
+           JOIN (SELECT date(time) as date, count(*) as total_error FROM log
+                 WHERE log.status = '404 NOT FOUND' GROUP BY date) as error
+           ON allrequests.date=error.date
+           WHERE round(error.total_error*100/allrequests.total_requests,2) > 1;
+       '''
+
+
+def get_query_result(query):
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    return result
+    db.close()
+
+
+def print_views(result):
+    for r in result:
+        print ('\t' + str(r[0]) + ' -- ' + str(r[1]) + ' views')
+
+
+def print_error(result):
+    for r in result:
+        dateOfError = datetime.datetime.strptime(str(r[0]), '%Y-%m-%d')
+        print ('\t' + dateOfError.strftime('%B %d, %Y') +
+        ' -- ' + "{0:0.1f}".format(r[1]) + '% error')
+
+
+print("1. What are the most popular three articles of all time?")
+query_result1 = get_query_result(query1)
+print_views(query_result1)
+
+print("2. Who are the most popular article authors of all time?")
+query_result2 = get_query_result(query2)
+print_views(query_result2)
+
+print("3. On which days did more than 1% of requests lead to errors?")
+query_result3 = get_query_result(query3)
+print_error(query_result3)
